a005f009b8c5947b62cbfbf00800823d5da28798
mhevyk <maxxevyk7@gmail.com>
2023-11-17 22:55:24 +0200

    Provided database connection before server starts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
+import { sequelize } from "./database/sequelize";
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,13 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(PORT, () => {
+function handleListen() {
   console.log(`Server started on http://localhost:${PORT}/`);
-});
+}
+
+async function start() {
+  await sequelize.authenticate();
+  app.listen(PORT, handleListen);
+}
+
+start();
--- server/src/types/environment.d.ts
+++ server/src/types/environment.d.ts
@@ -4,11 +4,13 @@ import type { Dialect } from "sequelize";
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      readonly PORT: number;
       readonly DB_USER: string;
       readonly DB_PASSWORD: string;
       readonly DB_HOST: string;
       readonly DB_DIALECT: Dialect;
       readonly DB_NAME: string;
+      readonly DB_PORT: number;
     }
   }
 }
