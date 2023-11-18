4f5a4b56a65827a939266d488403a2395a9cfb54
mhevyk <maxxevyk7@gmail.com>
2023-11-18 10:53:23 +0200

    Fixed database connection error

--- server/src/database/sequelize.ts
+++ server/src/database/sequelize.ts
@@ -4,7 +4,7 @@ import { Sequelize } from "sequelize-typescript";
 const MODELS_PATH = path.resolve(__dirname, "models");
 
 export const sequelize = new Sequelize({
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
