ff6550299df645fe85f24e57788960a4a533d42b
mhevyk <maxxevyk7@gmail.com>
2023-11-19 17:19:21 +0200

    Added basic login page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,3 +1,26 @@
+import { FormEvent } from "react";
+import { useLogin } from "../auth/useLogin";
+
 export function LoginPage() {
-  return <h1>Login</h1>;
+  const login = useLogin();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const credentials = {
+      username: formData.get("username") as string,
+      password: formData.get("password") as string,
+    };
+
+    await login(credentials);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="username" name="username" />
+      <input type="password" placeholder="password" name="password" />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
