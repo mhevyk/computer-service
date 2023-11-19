2137291b72d1015faa3a39175e6b6f35920a9888
mhevyk <maxxevyk7@gmail.com>
2023-11-19 17:12:38 +0200

    Configured auth api with interceptors and public api for request, that do not require attaching auth header and refreshing token

---
+++ client/src/axios/authApi.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import AuthService from "../services/AuthService";
+import UserStoreService from "../services/UserStoreService";
+import { BASE_URL } from "./common";
+
+export const $authApi = axios.create({
+  withCredentials: true,
+  baseURL: `${BASE_URL}/auth`,
+});
+
+$authApi.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${
+    UserStoreService.getValue()?.accessToken
+  }`;
+  return config;
+});
+
+$authApi.interceptors.response.use(
+  config => {
+    return config;
+  },
+  async error => {
+    const originalRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await AuthService.refresh();
+        UserStoreService.saveValue(response.data);
+        return $authApi.request(originalRequest);
+      } catch (e) {
+        console.log("Not auhorized");
+      }
+    }
+    throw error;
+  }
+);
--- client/src/services/AuthService.tsx
+++ client/src/services/AuthService.tsx
@@ -1,32 +1,22 @@
-import axios from "axios";
 import { AuthResponse } from "../types/auth";
-
-const BASE_URL = `http://localhost:5000`;
-
-const $authApi = axios.create({
-  withCredentials: true,
-  baseURL: BASE_URL,
-});
+import { $publicApi } from "../axios/publicApi";
+import { $authApi } from "../axios/authApi";
 
 export default class AuthService {
   static login(username: string, password: string) {
-    return $authApi.post<AuthResponse>("/auth/login", { username, password });
+    return $authApi.post<AuthResponse>("/login", { username, password });
   }
 
   static registration(username: string, password: string) {
-    return $authApi.post<AuthResponse>("/auth/registration", {
-      username,
-      password,
-    });
+    return $authApi.post<AuthResponse>("/registration", { username, password });
   }
 
   static logout() {
-    return $authApi.post<void>("/auth/logout");
+    return $authApi.post<void>("/logout");
   }
 
   static refresh() {
-    return axios.get<AuthResponse>(`${BASE_URL}/auth/refresh`, {
-      withCredentials: true,
-    });
+    // use public api to avoid calling interceptors
+    return $publicApi.get<AuthResponse>(`/refresh`, { withCredentials: true });
   }
 }
