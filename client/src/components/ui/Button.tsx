613115968c22e1673fd2c5a8c3c2fe1835b2ecae
mhevyk <maxxevyk7@gmail.com>
2023-11-27 16:23:20 +0200

    Added reusableButton component

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,11 +1,34 @@
-import { ComponentProps, PropsWithChildren } from "react";
+import { ComponentProps, PropsWithChildren, ReactNode } from "react";
+import { SIZES, Size, VARIANTS, Variant } from "../../types/common";
+import { cc } from "../../utils/cc";
+import { twMerge } from "tailwind-merge";
 
-type ButtonProps = PropsWithChildren & ComponentProps<"button">;
+type ButtonProps = PropsWithChildren &
+  ComponentProps<"button"> & {
+    variant?: Variant;
+    size?: Size;
+    icon?: ReactNode;
+  };
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({
+  children,
+  variant = "accent",
+  size = "md",
+  icon,
+  className,
+  ...props
+}: ButtonProps) {
+  const classes = cc(
+    "btn",
+    VARIANTS.includes(variant) && `btn-${variant}`,
+    SIZES.includes(size) && `btn-${size}`,
+    "flex-nowrap"
+  );
+
   return (
-    <button className="btn btn-accent" {...props}>
+    <button className={twMerge(classes, className)} {...props}>
       {children}
+      {icon}
     </button>
   );
 }
---
+++ client/src/types/common.ts
@@ -0,0 +1,5 @@
+export const VARIANTS = ["primary", "secondary", "accent", "ghost"] as const;
+export type Variant = (typeof VARIANTS)[number];
+
+export const SIZES = ["sm", "md", "lg"] as const;
+export type Size = (typeof SIZES)[number];
