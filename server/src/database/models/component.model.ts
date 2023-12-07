9f5bd121ccd2e677970c7b92e72d0d64685bb3a6
mhevyk <maxxevyk7@gmail.com>
2023-12-07 20:04:57 +0200

    server:feat:added component database model

---
+++ server/src/database/models/component.model.ts
@@ -0,0 +1,49 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  AutoIncrement,
+  AllowNull,
+  ForeignKey,
+  BelongsTo,
+} from "sequelize-typescript";
+import ComputerModel from "./computer.model";
+
+@Table({ tableName: "component" })
+export default class ComponentModel extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  component_id: number; // TODO: change to component_id
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  manufactorer_id: number;
+
+  @ForeignKey(() => ComputerModel)
+  @Column(DataType.INTEGER)
+  computer_id: number;
+
+  @Column(DataType.INTEGER)
+  processor_id: number;
+
+  @Column(DataType.INTEGER)
+  motherboard_id: number;
+
+  @Column(DataType.INTEGER)
+  memory_id: number;
+
+  @Column(DataType.INTEGER)
+  videocard_id: number;
+
+  @Column(DataType.INTEGER)
+  power_supply_id: number;
+
+  @Column(DataType.DOUBLE)
+  price_per_unit: number;
+
+  @Column(DataType.INTEGER)
+  quantity: number;
+}
